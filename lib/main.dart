ad2a3246daa3d9b360101ceddfe2b285c3527abd
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-26 11:57:01 +0530

    'new'

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,10 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Image.asset('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
           Expanded(
             flex: 1,
