8b24c0949275d1a9fd320e2a25fdd6010254986f
PrajwalMasulkar <prajwalmasulkar75@gmail.com>
2020-04-26 12:15:04 +0530

    'new'

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,21 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+                onPressed: (){
+
+                  print('Left button got pressed. ');
+                },
+                child: Image.asset('images/dice1.png'),
+
             ),
           ),
           Expanded(
             flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: FlatButton(child: Image.asset('images/dice1.png')),
-            ),
+
+              child: FlatButton(child: Image.asset('images/dice1.png'),
+              ),
+
           ),
 
         ],
